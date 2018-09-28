8ac1d7a59146f28a722c3fb59afaeeb6b132d8a6
Lucas Rojas <lucas.rojas@southworks.com>
2018-09-28 10:54:06 -0300

    modify the server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var serveStatic = require('serve-static');
 app = express();
 app.use('/', serveStatic(path.join(__dirname, "/dist")));
 
-var port = process.env.PORT || 8080;
-app.listen(port);
 
-console.log('Listening on port '+ port);
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});

dda21baa0eed34a209917f0974198baf18671b7a
Lucas Rojas <lucas.rojas@live.com.ar>
2019-08-11 00:47:40 -0300

    Deploy test

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var serveStatic = require('serve-static');
 var app = express();
 app.use('/', serveStatic(path.join(__dirname, "/dist")));
 
-var port = process.env.PORT || 8080;
-app.listen(port);
-
-console.log('Listening on port '+ port);
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Listening on: ", this.address().port)
+});

cd226a9660eb2987c50816b31be2689e4e98d8c0
Lucas Rojas <lucas.rojas@live.com.ar>
2019-08-11 00:20:49 -0300

    Fix express instance

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 
-app = express();
+var app = express();
 app.use('/', serveStatic(path.join(__dirname, "/dist")));
 
 var port = process.env.PORT || 8080;

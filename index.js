f57d77b370a50aff3bff21abff5b228f1c124ac9
Lucas Rojas <lucas.rojas@live.com.ar>
2019-08-11 01:30:26 -0300

    Restore express

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ var serveStatic = require('serve-static');
 
 var app = express();
 
-app.use(serveStatic(path.join(__dirname, "/dist")));
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, "/dist"))
-})
+app.use('/',serveStatic(path.join(__dirname, "/dist")));
+
 app.listen(process.env.PORT || 8080, function(){
     console.log("Listening on: ", this.address().port)
 });

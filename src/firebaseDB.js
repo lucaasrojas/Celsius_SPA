6737a1237af41c6410da66e6aae7d6e43fbf26e2
Lucas <lucas.rojas@live.com.ar>
2019-08-28 00:26:56 -0300

    Add console log to debug

--- src/firebaseDB.js
+++ src/firebaseDB.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
 import config from './config';
+console.log("config",config)
 let app = firebase.initializeApp(config.MainDB);
 export default {
     db: app.database()

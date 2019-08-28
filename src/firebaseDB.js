bbe477bffff5366888a0cd1478b781764cebcd97
Lucas <lucas.rojas@live.com.ar>
2019-08-28 00:30:06 -0300

    remove console log

--- src/firebaseDB.js
+++ src/firebaseDB.js
@@ -1,6 +1,5 @@
 import firebase from 'firebase';
 import config from './config';
-console.log("config",config)
 let app = firebase.initializeApp(config.MainDB);
 export default {
     db: app.database()

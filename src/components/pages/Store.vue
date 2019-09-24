331a4980c6fff05e07416f2fc8408e8248c4ff76
unknown <lucas.rojas@live.com.ar>
2019-09-23 21:22:51 -0300

    Add gallery with filter and animation

--- src/components/pages/Store.vue
+++ src/components/pages/Store.vue
@@ -1,13 +1,92 @@
 <template>
-  <h1> Welcome to the store</h1>
+<div>
+<div class="row">
+
+  <div class="col-md-12 d-flex justify-content-center mb-5">
+
+    <button type="button" class="btn btn-outline-black waves-effect filter text-white" @click="btnClicked($event)" data-rel="all">All</button>
+    <button type="button" id="btnMountains" class="btn btn-outline-black waves-effect filter text-white" @click="btnClicked($event)" data-rel="1">Mountains</button>
+    <button type="button" class="btn btn-outline-black waves-effect filter text-white" @click="btnClicked($event)" data-rel="2">Sea</button>
+
+  </div>
+
+</div>
+<div class="gallery" id="gallery">
+
+  <div class="mb-3 pics animation all 2">
+    <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(73).jpg" alt="Card image cap">
+  </div>
+  <div class="mb-3 pics animation all 1">
+    <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Vertical/mountain1.jpg" alt="Card image cap">
+  </div>
+  <div class="mb-3 pics animation all 1">
+    <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Vertical/mountain2.jpg" alt="Card image cap">
+  </div>
+  <div class="mb-3 pics animation all 2">
+    <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg" alt="Card image cap">
+  </div>
+  <div class="mb-3 pics animation all 2">
+    <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg" alt="Card image cap">
+  </div>
+  <div class="mb-3 pics animation all 1">
+    <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Vertical/mountain3.jpg" alt="Card image cap">
+  </div>
+</div>
+</div>
+
 </template>
 
 <script>
-export default {
+import $ from 'jquery'
 
+export default {
+  methods: {
+    btnClicked: (btn) => {
+      let selectedClass = btn.srcElement.dataset.rel;
+      $("#gallery").fadeTo(100, 0.1);
+      $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+      setTimeout(function() {
+        $("."+selectedClass).fadeIn().addClass('animation');
+        $("#gallery").fadeTo(300, 1);
+      }, 300);
+    }
+  }
 }
 </script>
 
 <style>
+
+.gallery {
+-webkit-column-count: 3;
+-moz-column-count: 3;
+column-count: 3;
+-webkit-column-width: 33%;
+-moz-column-width: 33%;
+column-width: 33%; }
+.gallery .pics {
+-webkit-transition: all 350ms ease;
+transition: all 350ms ease; }
+.gallery .animation {
+-webkit-transform: scale(1);
+-ms-transform: scale(1);
+transform: scale(1); }
+
+@media (max-width: 450px) {
+.gallery {
+-webkit-column-count: 1;
+-moz-column-count: 1;
+column-count: 1;
+-webkit-column-width: 100%;
+-moz-column-width: 100%;
+column-width: 100%;
+}
+}
+
+@media (max-width: 400px) {
+.btn.filter {
+padding-left: 1.1rem;
+padding-right: 1.1rem;
+}
+}
 
 </style>

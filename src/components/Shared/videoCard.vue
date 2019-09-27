c7888eb3841aae23573ddf24c4fc38064262d356
unknown <lucas.rojas@live.com.ar>
2019-09-27 12:29:32 -0300

    Fix video card size

--- src/components/Shared/videoCard.vue
+++ src/components/Shared/videoCard.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="container">
+    <div>
         <div class="row my-2" v-for="video in itemsList" v-bind:key="video.id">
-           <div class="card mx-auto text-white bg-dark">
+            <div class="col-md-12">
+            
+                <div class="card mx-auto text-white bg-dark">
             <h5 class="card-header">{{video.title}}</h5>
             <div class="card-body">
-                <iframe width="560" height="315" :src=video.url frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <iframe width="100%" height="" :src=video.url frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
             </div>
+           </div>
+            
             </div>
+           
         </div>
     </div>
 </template>
@@ -28,6 +33,10 @@ iframe {
     border-width: 5px;
     border-color:blanchedalmond;
     border-radius: 5px;
+    width: 100vw; 
+    height: 56.25vw; /* 100/56.25 = 560/315 = 1.778 */
+    max-width: 100%;
+    max-height: calc((100vw - 40px) / (16/9));
 }
 
 .subtitulo {

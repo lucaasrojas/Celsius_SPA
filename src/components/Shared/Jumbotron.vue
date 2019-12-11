9765a7172db508b693e0bb9497373ff77dcce855
Lucas Rojas <lucas.rojas@southworks.com>
2019-12-11 15:57:30 -0300

    Add background image to jumbotron and center the content

--- src/components/Shared/Jumbotron.vue
+++ src/components/Shared/Jumbotron.vue
@@ -3,11 +3,11 @@
         <navbar></navbar>
         <div class="jumbotron">
             <div class="row jumbotron-content">
-                <div class="col-lg-12">
-                <p class="mainTitle">{{mainTitle}}</p>
-                    <p class="mainSubtitle">
-                        {{mainDescription}}
-                    </p>
+                <div class="col-lg-12 mx-auto my-auto">
+                    <p class="mainTitle">{{mainTitle}}</p>
+                        <p class="mainSubtitle">
+                            {{mainDescription}}
+                        </p>
                 </div>
             </div>
         </div>
@@ -29,12 +29,19 @@ export default {
 
 .jumbotron-content {
     color: white;
+    height: 100%;
+    width: 100%;
+
 }
 .jumbotron {
+    height: 300px;
+    background-image: linear-gradient(to bottom, rgba(148, 148, 148, 0.6) 0%,rgba(148,148,148,0.9) 100%), url('https://images.unsplash.com/photo-1523457938575-6e66e08ff567?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1457&q=80');
+    background-size: fill;
+    background-position: center;
     position:relative;
-    background-color: transparent !important;
     margin: 0;
-    padding: 1rem !important;    
+    padding: 1rem !important;   
+   
 }
 
 .mainTitle {

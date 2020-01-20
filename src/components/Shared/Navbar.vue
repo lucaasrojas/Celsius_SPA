171fdd85a343a586e71a11da0352341861a8dae8
unknown <lucas.rojas@live.com.ar>
2020-01-20 20:49:05 -0300

    Change name and icon

--- src/components/Shared/Navbar.vue
+++ src/components/Shared/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <nav class="navbar navbar-expand-lg navbar-dark">
-        <router-link  class="navbar-brand navbar-element-padding" style="position: relative" to="/"><strong>Celsius</strong></router-link>
+        <router-link  class="navbar-brand navbar-element-padding" style="position: relative" to="/"><strong>TakeACoffee</strong></router-link>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>

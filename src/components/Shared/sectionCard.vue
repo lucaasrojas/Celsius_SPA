bf798dd0c2640ec309c89d625080f7221185d080
unknown <lucas.rojas@live.com.ar>
2020-01-20 23:04:32 -0300

    Add shadow to section circles

--- src/components/Shared/sectionCard.vue
+++ src/components/Shared/sectionCard.vue
@@ -26,15 +26,16 @@ props: ['section'],
 }
 
 .icon {
-	font-size: 7rem;
+	font-size: 7em;
 	margin: 2rem;
+	max-width: 100px;
     color:white;
 }
 
 .sectionCircle {
 	margin: 10px;
-    max-width: 200px;
     border: 1px solid;
     border-color: rgba(133, 133, 133, 0.459);
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 </style>

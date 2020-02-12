05a26fabc04a57893beac1b1665c7713fe9953e4
Lucas Rojas <lucas.rojas@southworks.com>
2020-02-12 18:46:54 -0300

    remove rewrites

--- index.js
+++ index.js
@@ -5,28 +5,8 @@ var history = require('connect-history-api-fallback');
 
 var app = express();
 
-app.use(history({
-    index: 'index.html',
-    rewrites: [
-        { from: /\//, to: '/home'},
-        { from: /\/home/, to: '/home'},
-        { from: /\/musica/, to: 'musica'},
-        { from: /\/peliculas/, to: '/peliculas'},
-        { from: /\/galerias/, to: 'galerias'},
-        { from: /\/store/, to: 'store'},
-        { from: /\/educativo/, to: 'educativo'},
-        { from: /\/listacontador/, to: 'listacontador'},
-        { from: /\/todolist/, to: 'todolist'},
-        { from: /\/usuarios/, to: '/usuarios'},
-        { from: /\/spotifyList/, to: 'spotifyList'},
-        { from: /\/recetas/, to: '/recetas'},
-        { from: /\/admin/, to: '/admin'},
-        { from: /\/product/, to: '/product'},
-        { from: /\/videoCard/, to: '/videoCard'},
-        { from: /\/adminPanel/, to: '/adminPanel'},
-      ]
-}));
-
+app.use(history());
+app.use(express.static(__dirname))
 app.listen(process.env.PORT || 8080, function(){
     console.log("Listening on: ", this.address().port)
 });

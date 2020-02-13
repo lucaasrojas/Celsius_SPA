57caef5f6ac47cc057eb639da68cece1cdee642d
unknown <lucas.rojas@live.com.ar>
2020-02-12 21:15:45 -0300

    Apply middleware changes

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var history = require('connect-history-api-fallback');
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
-var app = express();
+const app = express();
 
-app.use(history());
-app.use(express.static(__dirname))
-app.listen(process.env.PORT || 8080, function(){
-    console.log("Listening on: ", this.address().port)
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });

0667e5b8583c1231edc02fb953fa7421b159929d
unknown <lucas.rojas@live.com.ar>
2020-07-12 00:51:59 -0300

    Create Dolar component

---
+++ src/components/Shared/dolar.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="dolar">
+        <h1>{{ title }}</h1>
+        <div class="row">
+            <div v-for="(dolar, index) of apiResponse" :key="index" 
+            class="col-sm-10 col-md-4 card border-dark mb-3 cotizacion">
+                <h2 class="card-header">
+                    {{dolar.casa.nombre}}
+                </h2>
+                <div class="col card-body text-dark">
+                    <div class="row"> 
+                        <div class="col"> 
+                            <h3 class="card-title">Venta</h3>
+                            <span class="card-text">
+                                {{dolar.casa.venta}}
+                            </span>
+                        </div>
+                        <div class="col"> 
+                            <h3 class="card-title">Compra</h3>
+                            <span class="card-text">
+                                {{dolar.casa.compra}}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "Cotizacion Dolar",
+            apiResponse: {}        
+        }
+    },
+    mounted(){
+        fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+        .then(async res => {
+            this.apiResponse = await res.clone().json()
+        })
+    }
+
+}
+</script>
+
+<style>
+.card.cotizacion {
+    padding: 0;
+    margin: 1rem;
+    display: inline-block;
+}
+
+.dolar{
+    text-align: center;
+}
+
+.dolar > div {
+    display: inline-block;
+}
+</style>
---
+++ src/components/Shared/instagram.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+	<div class="row">
+		<div class="col">
+			<h1>{{user}}</h1>
+		</div>
+	</div>
+    <div class="row">
+        <div class="col" v-for="(image,index) in images" :key="index">
+            <h1></h1>
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" :src="`${image.node.display_url}`" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">{{image.node.edge_media_to_caption.edges[0].node.text}}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+	</div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            images: [],
+            user: ''
+        }
+    },
+    methods: {
+        getInstagramData() {
+            fetch('https://www.instagram.com/boywithtools')
+            .then(async res => {
+                const response = await res.json();
+                console.log("instagram", response)
+            })
+        }
+    },
+    mounted(){
+        this.getInstagramData();
+    }
+}
+</script>
+
+<style>
+
+</style>
